7bf5d03f3a1e98a5ac7de0c18516d11f6f649a0c
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2020-06-27 22:13:57 -0300

    Initializing Project

---
+++ iOS-challenge/Pods/Target Support Files/Pods-iOS-challenge-iOS-challengeUITests/Pods-iOS-challenge-iOS-challengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_challenge_iOS_challengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_challenge_iOS_challengeUITests
+@end
---
+++ iOS-challenge/iOS-challenge/Models/Repository.swift
@@ -0,0 +1,9 @@
+//
+//  Repository.swift
+//  iOS-challenge
+//
+//  Created by Gabriel Faria on 6/27/20.
+//  Copyright © 2020 Gabriel Rodrigues. All rights reserved.
+//
+
+import Foundation
